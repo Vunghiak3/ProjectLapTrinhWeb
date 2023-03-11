b4c0a0b0253515af78f89b75dca52692f41dbc9f
your name <abc@gmail.com>
2023-03-12 00:28:47 +0700

    delete file in templates

--- Templates/menu.js
+++
@@ -1,8 +0,0 @@
-const progressbar = document.querySelector(".progress-bar");
-const bar = document.querySelector(".bar");
-const showmenu = document.querySelector(".menu-mobi");
-
-progressbar.addEventListener("click", () => {
-  bar.classList.toggle("rotate-bar");
-  showmenu.classList.toggle("show-menu");
-});
